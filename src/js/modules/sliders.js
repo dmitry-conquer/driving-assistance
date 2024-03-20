dbb067bb8856032285e323cb4e47350e8b376fde
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-03-20 12:36:12 +0200

    Сладйер відеокоментарі

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -61,3 +61,35 @@ if (quotesSlider) {
     },
   });
 }
+
+const commentsSlider = document.querySelector(".comments-slider");
+if (commentsSlider) {
+  new Swiper(commentsSlider, {
+    modules: [Pagination, Navigation],
+    wrapperClass: "comments-slider-wrapper",
+    slideClass: "comments-slider-slide",
+    slidesPerView: 4,
+    spaceBetween: 30,
+    speed: 1200,
+
+    pagination: {
+      el: ".pagination-comments",
+      clickable: true,
+    },
+    navigation: {
+      prevEl: ".comments-prev",
+      nextEl: ".comments-next",
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 12,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    },
+  });
+}
